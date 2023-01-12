2c7514025e5b3a56487c958aa54e9d6564ac0db4
ranjan gowda <dranjanjgowda@gmail.com>
2023-01-12 13:10:04 +0530

    basic operations

--- basicOperation.js
+++ basicOperation.js
@@ -2,3 +2,7 @@ const age = 10;
 
 const drink = age >= 18 ? "wine" : "water"
 console.log(drink)
+
+const bill = 275;
+const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 20;
+console.log(`The bill was ${bill} , the tip is ${tip} and the total value is ${bill + tip}`)
